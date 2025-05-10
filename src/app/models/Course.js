9043bf5d8afb99f62ef83221e82058d628fe22a8
Nguyen Hoang Anh <anhnhtech@gmail.com>
2025-05-10 16:04:44 +0700

    add create update detele

---
+++ src/app/models/Course.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+// const slug = require('mongoose-slug-generator');
+// mongoose.plugin(slug);
+const slugify = require('slugify');
+const Schema = mongoose.Schema;
+
+
+const Course = new Schema({
+    name: { type: String, required: true},
+    description: { type: String, maxLength: 600 },
+    image: { type: String, maxLength: 255 },
+    keyvideo: { type: String, required: true, maxLength: 255 },
+    level: { type: String, maxLength: 255 },
+    slug: { type: String, unique: true },
+  },{
+    timestamps: true,
+  });
+
+  // Tạo slug trước khi lưu
+Course.pre('save', function (next) {
+  if (!this.slug && this.name) {
+    this.slug = slugify(this.name, { lower: true, strict: true });
+  }
+  next();
+});
+
+  module.exports = mongoose.model('Course', Course);
---
+++ src/config/db/index.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+async function connect(){
+
+    try {
+        await mongoose.connect('mongodb://localhost:27017/blog_dev');
+        console.log('MongoDB connected successfully');
+    } catch (error) {
+        console.error('MongoDB connection failed:');
+    }
+}
+
+module.exports = {connect};
